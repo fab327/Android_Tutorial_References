c28e7146a019737e7062746a9ede53d75ced61c8
f@b <spiritualsilver@gmail.com>
2018-08-14 20:24:26 -0400

    Update to City guide

--- 3_CityGuide/app/src/main/java/com/justfabcodes/cityguide/MapsActivity.kt
+++ 3_CityGuide/app/src/main/java/com/justfabcodes/cityguide/MapsActivity.kt
@@ -1,26 +1,34 @@
 package com.justfabcodes.cityguide
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.pm.PackageManager
 import android.os.Bundle
-
+import android.support.design.widget.Snackbar
+import android.support.v4.app.ActivityCompat
+import android.support.v7.app.AlertDialog
+import android.support.v7.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.activity_maps.*
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private val LOCATION_REQUEST = 1;
+
+    private lateinit var mapFragment: SupportMapFragment
     private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        mapFragment = map as SupportMapFragment
+
+        checkPermissionAndStart()
     }
 
     /**
@@ -35,9 +43,58 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        //Add a marker in Sydney and move the camera
+        val sydneyLatLng = LatLng(-34.0, 151.0)
+        val sydneyMarker = MarkerOptions().position(sydneyLatLng).title("Marker in Sydney")
+        mMap.addMarker(sydneyMarker)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydneyLatLng))
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            LOCATION_REQUEST -> {
+                if (grantResults.isNotEmpty() &&
+                        permissions[0] == ACCESS_FINE_LOCATION &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    instantiateMap()
+                } else {
+                    mapFragment.view?.let {
+                        Snackbar.make(it, "Permission hasn't been granted", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            }
+
+            else -> {
+                // No other permission request for now
+            }
+        }
+    }
+
+    private fun instantiateMap() {
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment.getMapAsync(this)
+        mapFragment.view
+    }
+
+    private fun checkPermissionAndStart() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //If permission has been denied once before
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, ACCESS_FINE_LOCATION)) {
+                AlertDialog.Builder(this)
+                        .setTitle("Location Request")
+                        .setMessage("We need access to your location")
+                        .setPositiveButton("Ok", { dialog, which ->
+                            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), LOCATION_REQUEST)
+                        })
+                        .setNegativeButton("No thanks", null)
+                        .create()
+                        .show()
+            } else {
+                ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), LOCATION_REQUEST)
+            }
+        } else {
+            instantiateMap()
+        }
+    }
+
 }
