7c95683af5948c35a3c8f09bfd8fb48b826cdba7
f@b <spiritualsilver@gmail.com>
2018-08-17 20:09:28 -0400

    Update to City guide

--- 3_CityGuide/app/src/main/java/com/justfabcodes/cityguide/MapsActivity.kt
+++ 3_CityGuide/app/src/main/java/com/justfabcodes/cityguide/MapsActivity.kt
@@ -1,58 +1,106 @@
 package com.justfabcodes.cityguide
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException
+import com.google.android.gms.common.GooglePlayServicesRepairableException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.location.places.ui.PlacePicker
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import kotlinx.android.synthetic.main.activity_maps.*
+import java.io.IOException
 
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-    private val LOCATION_REQUEST = 1;
+    private val LOCATION_REQUEST_PERMISSION = 1
+    private val LOCATION_UPDATE_SETTING_CHECK = 2
+    private val PLACE_PICKER_REQUEST = 3
 
     private lateinit var mapFragment: SupportMapFragment
-    private lateinit var mMap: GoogleMap
+    private lateinit var map: GoogleMap
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var lastLocation: Location
+
+    //Location update
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        mapFragment = map as SupportMapFragment
+        mapFragment = mapFragmentView as SupportMapFragment
+        fab.setOnClickListener {
+            loadPlacePicker()
+        }
 
+        instantiateLocationFuse()
         checkPermissionAndStart()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult?) {
+                super.onLocationResult(p0)
+
+                p0?.let { lastLocation = it.lastLocation }
+                placeMarkerOnMap(LatLng(lastLocation.latitude, lastLocation.longitude))
+            }
+        }
+        createLocationRequest()
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+    override fun onResume() {
+        super.onResume()
+        if (!locationUpdateState) {
+            startLocationUpdate()
+        }
+    }
 
-        //Add a marker in Sydney and move the camera
-        val sydneyLatLng = LatLng(-34.0, 151.0)
-        val sydneyMarker = MarkerOptions().position(sydneyLatLng).title("Marker in Sydney")
-        mMap.addMarker(sydneyMarker)
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydneyLatLng))
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == LOCATION_UPDATE_SETTING_CHECK && resultCode == Activity.RESULT_OK) {
+            locationUpdateState = true
+            startLocationUpdate()
+        } else if (requestCode == PLACE_PICKER_REQUEST && resultCode == Activity.RESULT_OK) {
+            val place = PlacePicker.getPlace(this, data)
+            var addressText = place.name.toString()
+            addressText += "\n".plus(place.address.toString())
+
+            placeMarkerOnMap(place.latLng)
+        }
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         when (requestCode) {
-            LOCATION_REQUEST -> {
+            LOCATION_REQUEST_PERMISSION -> {
                 if (grantResults.isNotEmpty() &&
                         permissions[0] == ACCESS_FINE_LOCATION &&
                         grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -70,12 +118,61 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+
+        map.uiSettings.isZoomControlsEnabled = true
+        map.setOnMarkerClickListener(this)
+
+        setUpMap()
+    }
+
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        return false
+    }
+
     private fun instantiateMap() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment.getMapAsync(this)
         mapFragment.view
     }
 
+    @SuppressLint("MissingPermission")
+    private fun setUpMap() {
+        map.isMyLocationEnabled = true
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->
+            location?.let {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                placeMarkerOnMap(currentLatLng)
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun instantiateLocationFuse() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdate() {
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    /*
+     * Ask for location permission
+     */
     private fun checkPermissionAndStart() {
         if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             //If permission has been denied once before
@@ -84,16 +181,103 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                         .setTitle("Location Request")
                         .setMessage("We need access to your location")
                         .setPositiveButton("Ok", { dialog, which ->
-                            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), LOCATION_REQUEST)
+                            ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), LOCATION_REQUEST_PERMISSION)
                         })
                         .setNegativeButton("No thanks", null)
                         .create()
                         .show()
             } else {
-                ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), LOCATION_REQUEST)
+                ActivityCompat.requestPermissions(this, arrayOf(ACCESS_FINE_LOCATION), LOCATION_REQUEST_PERMISSION)
             }
         } else {
             instantiateMap()
+        }
+    }
+
+    /*
+     * Map pins
+     */
+    private fun placeMarkerOnMap(location: LatLng) {
+        val markerOptions = MarkerOptions().position(location)
+
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+        //markerOptions.icon(BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)))
+
+        markerOptions.title(getAddress(location))
+        map.addMarker(markerOptions)
+        map.mapType = GoogleMap.MAP_TYPE_TERRAIN
+    }
+
+    /*
+     * Geocoding (get address from location)
+     */
+    private fun getAddress(latLng: LatLng): String {
+        val geocoder = Geocoder(this)
+        val addresses: List<Address>?
+        val address: Address?
+        var addressText = StringBuilder()
+
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+
+            if (addresses != null && addresses.isNotEmpty()) {
+                address = addresses[0]
+                for (i in 0 until address.maxAddressLineIndex + 1) {
+                    addressText.append( if (i == 0) address.getAddressLine(i) else "\n" + address.getAddressLine(i) )
+                }
+            }
+        } catch (e: IOException) {
+            Log.e("MapsActivity", e.localizedMessage)
+        }
+
+        return addressText.toString()
+    }
+
+    /*
+     * Location update
+     */
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000        // 10 seconds
+        locationRequest.fastestInterval = 5000  // 5 seconds
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+
+        val client = LocationServices.getSettingsClient(this)
+        val task = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener {
+            locationUpdateState = true
+            startLocationUpdate()
+        }
+        task.addOnFailureListener { e ->
+            if (e is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    e.startResolutionForResult(this@MapsActivity, LOCATION_UPDATE_SETTING_CHECK)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    /*
+     * Place picker with a built-in UI component provided by the support library
+     */
+    private fun loadPlacePicker() {
+        val builder = PlacePicker.IntentBuilder()
+
+        try {
+            startActivityForResult(builder.build(this), PLACE_PICKER_REQUEST)
+        } catch (e: GooglePlayServicesRepairableException) {
+            //Ignore error
+        } catch (e: GooglePlayServicesNotAvailableException) {
+            //Ignore error
         }
     }
 
