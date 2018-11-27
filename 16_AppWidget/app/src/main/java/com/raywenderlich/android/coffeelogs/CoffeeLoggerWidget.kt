438ca652c6002c89497108bceaadf0cab98e80e4
fab <spiritualsilver@gmail.com>
2018-11-27 18:00:27 -0500

    App widget tutorial commit

---
+++ 16_AppWidget/app/src/main/java/com/raywenderlich/android/coffeelogs/CoffeeLoggerWidget.kt
@@ -0,0 +1,81 @@
+package com.raywenderlich.android.coffeelogs
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in [CoffeeLoggerWidgetConfigureActivity]
+ */
+class CoffeeLoggerWidget : AppWidgetProvider() {
+
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+//        for (appWidgetId in appWidgetIds) {
+//            updateAppWidget(context, appWidgetManager, appWidgetId)
+//        }
+
+        // We can run the update via a service as well
+        val intent = Intent(context.applicationContext, CoffeeQuotesService::class.java)
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds)
+        context.startService(intent)
+    }
+
+    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
+        // When the user deletes the widget, delete the preference associated with it.
+        for (appWidgetId in appWidgetIds) {
+            CoffeeLoggerPersistence(context).deletePref(appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    companion object {
+        internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+
+            val coffeeLoggerPersistence = CoffeeLoggerPersistence(context)
+            val widgetText = coffeeLoggerPersistence.loadTitlePref().toString()
+            val limit = coffeeLoggerPersistence.getLimitPref(appWidgetId)
+            val background = if (limit <= widgetText.toInt()) R.drawable.background_overlimit else R.drawable.background
+
+            // Construct the RemoteViews object & update UI
+            val views = RemoteViews(context.packageName, R.layout.coffee_logger_widget)
+            views.setTextViewText(R.id.appwidget_text, widgetText)
+            views.setTextViewText(R.id.coffee_quote, getRandomQuote(context))
+            views.setInt(R.id.widget_layout, "setBackgroundResource", background)
+
+            // Register the click listeners
+            views.setOnClickPendingIntent(R.id.ristretto_button, getPendingIntent(context, CoffeeTypes.RISTRETTO.grams))
+            views.setOnClickPendingIntent(R.id.espresso_button, getPendingIntent(context, CoffeeTypes.ESPRESSO.grams))
+            views.setOnClickPendingIntent(R.id.long_button, getPendingIntent(context, CoffeeTypes.LONG.grams))
+
+            // Instruct the widget manager to update the widget
+            appWidgetManager.updateAppWidget(appWidgetId, views)
+        }
+
+        private fun getPendingIntent(context: Context, value: Int): PendingIntent {
+            val intent = Intent(context, MainActivity::class.java)
+            intent.action = Constants.ADD_COFFEE_INTENT
+            intent.putExtra(Constants.GRAMS_EXTRA, value)
+
+            return PendingIntent.getActivity(context, value, intent, 0)
+        }
+
+        private fun getRandomQuote(context: Context): String {
+            val quotes = context.resources.getStringArray(R.array.coffee_texts)
+            val rand = Math.random() * quotes.size
+            return quotes[rand.toInt()].toString()
+        }
+    }
+}
+
---
+++ 16_AppWidget/app/src/main/java/com/raywenderlich/android/coffeelogs/CoffeeLoggerWidgetConfigureActivity.kt
@@ -0,0 +1,66 @@
+package com.raywenderlich.android.coffeelogs
+
+import android.app.Activity
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+
+/**
+ * The configuration screen for the [CoffeeLoggerWidget] AppWidget.
+ */
+class CoffeeLoggerWidgetConfigureActivity : Activity() {
+
+    internal val coffeeLoggerPersistence = CoffeeLoggerPersistence(this)
+    internal var mAppWidgetId = AppWidgetManager.INVALID_APPWIDGET_ID
+    internal lateinit var mAppWidgetText: EditText
+
+    internal var mOnClickListener: View.OnClickListener = View.OnClickListener {
+        val context = this@CoffeeLoggerWidgetConfigureActivity
+
+        // When the button is clicked, store the string locally
+        val widgetText = mAppWidgetText.text.toString()
+        coffeeLoggerPersistence.saveLimitPref(widgetText.toInt(), mAppWidgetId)
+
+        // It is the responsibility of the configuration activity to update the app widget
+        val appWidgetManager = AppWidgetManager.getInstance(context)
+        CoffeeLoggerWidget.updateAppWidget(context, appWidgetManager, mAppWidgetId)
+
+        // Make sure we pass back the original appWidgetId
+        val resultValue = Intent()
+        resultValue.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, mAppWidgetId)
+        setResult(Activity.RESULT_OK, resultValue)
+        finish()
+    }
+
+    public override fun onCreate(icicle: Bundle?) {
+        super.onCreate(icicle)
+
+        // Set the result to CANCELED.  This will cause the widget host to cancel
+        // out of the widget placement if the user presses the back button.
+        setResult(Activity.RESULT_CANCELED)
+
+        setContentView(R.layout.coffee_logger_widget_configure)
+        mAppWidgetText = findViewById<View>(R.id.appwidget_text) as EditText
+        findViewById<View>(R.id.add_button).setOnClickListener(mOnClickListener)
+
+        // Find the widget id from the intent.
+        val intent = intent
+        val extras = intent.extras
+        if (extras != null) {
+            mAppWidgetId = extras.getInt(
+                    AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
+        }
+
+        // If this activity was started with an intent without an app widget ID, finish with an error.
+        if (mAppWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
+            finish()
+            return
+        }
+    }
+
+}
+
