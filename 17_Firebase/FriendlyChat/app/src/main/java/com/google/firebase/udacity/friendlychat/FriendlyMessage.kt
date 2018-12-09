fde5fe3b298773cae779d4e78d8db2351984523c
f@b <spiritualsilver@gmail.com>
2018-12-09 16:58:11 -0500

    Friendly Chat app
    - committing progress from mbp

--- 17_Firebase/FriendlyChat/app/src/main/java/com/google/firebase/udacity/friendlychat/FriendlyMessage.kt
+++ 17_Firebase/FriendlyChat/app/src/main/java/com/google/firebase/udacity/friendlychat/FriendlyMessage.kt
@@ -1,3 +1,6 @@
 package com.google.firebase.udacity.friendlychat
 
-data class FriendlyMessage(var text: String?, var name: String?, var photoUrl: String?)
+data class FriendlyMessage(var text: String?, var name: String?, var photoUrl: String?) {
+    // Empty constructor required for firebase
+    constructor() : this(null, null, null)
+}
